e8e0d20c800bc573d34d4970f843afdd7bb599d6
learnerone <Sue.Han@seattlecolleges.edu>
2017-06-15 15:57:18 -0700

    Add files via upload

---
+++ Controllers/Donation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCAssignment7_1.Models;
+
+namespace MVCAssignment7_1.Controllers
+{
+    public class Donation : Controller
+    {
+        private Community_AssistEntities db = new Community_AssistEntities();
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DonationAmount,PersonKey,DonationDate")] MVCAssignment7_1.Models.Donation donation)
+        {
+            if (ModelState.IsValid)
+            {        
+                db.Donations.Add(donation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }      
+    }
+}
